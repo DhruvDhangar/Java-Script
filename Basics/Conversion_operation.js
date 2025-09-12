3664a498f6c92c73e7a1c84bbc39ecbb01a7f418
Dhruv Dhangar <112796265+DhruvDhangar@users.noreply.github.com>
2025-09-12 12:06:17 +0000

    Learned Conversion of Data-types

---
+++ Basics/Conversion_operation.js
@@ -0,0 +1,47 @@
+let character = "";
+
+let charToNumber = Number(character);
+
+// console.log(charToNumber);
+// console.log(typeof charToNumber);
+
+/*
+    String to Number Conversion.
+
+    "33" => Number (33)
+    "33abs" => Nan
+    "" => Number (0)
+
+*/
+
+let str = " ";
+
+let strToBool = Boolean(str);
+
+// console.log(strToBool);
+// console.log(typeof strToBool);
+
+/*
+    String to Boolean Conversion.
+
+    "XyZ" => true
+    "" => false
+    " " => true
+*/
+
+let isLogIn = NaN;
+
+let NumToBool = Boolean(isLogIn);
+
+console.log(NumToBool);
+console.log(typeof NumToBool);
+
+/*
+    Number to bool Conversion.
+
+    .... -2, -1, 1, 2, .... => true
+    0 => false
+    null => false
+    undefined => false
+    NaN => false
+*/
