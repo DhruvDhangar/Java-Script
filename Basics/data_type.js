1d8e128c6504e07f627f6b6b82501caab8310830
Dhruv Dhangar <112796265+DhruvDhangar@users.noreply.github.com>
2025-09-12 11:37:24 +0000

    Learned Data types

---
+++ Basics/data_type.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer vversion
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+// data types
+/*
+    number, bigint
+    string
+    boolean
+    null => standalone value
+    undefined => 
+    symbol => unique
+*/
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
+// Non - premitive data type. ==> Object, array 
