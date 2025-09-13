6721e535ebd6e660a6bcc533305eb249407f7627
Dhruv Dhangar <112796265+DhruvDhangar@users.noreply.github.com>
2025-09-13 11:36:05 +0000

    Learned about heap and stack in JS.

--- Basics/data_type.js
+++ Basics/data_type.js
@@ -12,8 +12,8 @@
     symbol => unique
 */
 
-console.log(typeof null) // object
-console.log(typeof undefined) // undefined
+// console.log(typeof null) // object
+// console.log(typeof undefined) // undefined
 
 let loginId = Symbol('123')
 let sessionId = Symbol('123')
@@ -21,8 +21,8 @@ let sessionId = Symbol('123')
 console.log(loginId == sessionId);
 // Both the symbols are different no matter if it's values are same or not.
 
-console.log(typeof loginId);
-console.log(sessionId);
+// console.log(typeof loginId);
+// console.log(sessionId);
 
 // Non - premitive data type. ==> Object, array, functions
 
@@ -38,6 +38,31 @@ let tryIt = function(){
     console.log("Hello JS");
 }
 
-console.log(typeof tryIt);
-console.log(typeof fun);
-console.log(typeof heros);
+// console.log(typeof tryIt);
+// console.log(typeof fun);
+// console.log(typeof heros);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Stack(primitive) & Heap(Non - primitive)
+
+let myaccount = "Dhruv Dhangar"
+let anotheraccount = myaccount
+
+anotheraccount = "Dhangar Dhruv"
+
+console.log(myaccount);
+console.log(anotheraccount);
+
+
+let Obj1 = {
+    name: "Dhruv",
+    age: 21,
+    upi: "user@yespop"
+}
+
+let Obj2 = Obj1
+
+Obj2.upi = "Hello@yespop"
+
+console.log(Obj1.upi); // Both the values are changed
+console.log(Obj2.upi); // beacause they are using heap --> pass by reference 
