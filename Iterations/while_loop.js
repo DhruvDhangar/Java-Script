eba73057b8308e0cb9d71599f4cc832e308576d6
Dhruv Dhangar <112796265+DhruvDhangar@users.noreply.github.com>
2025-10-08 18:06:14 +0000

    Learned about while/do-while loops

---
+++ Iterations/while_loop.js
@@ -0,0 +1,15 @@
+// while loop
+
+let i = 6
+
+// while (i <= 5) {
+//     console.log(`value of i is ${i}`);
+//     i++
+// }
+
+// do-while loop works once if the condition is false no matter.
+
+do{
+    console.log(i);
+    i++;
+}while(i <= 5);
