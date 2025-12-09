724c205d8f76aea02f7ed3021468070058c227bb
Dhruv Dhangar <112796265+DhruvDhangar@users.noreply.github.com>
2025-12-09 12:50:45 +0000

    Learned about filter property of Array.

--- Iterations/Other_loops.js
+++ Iterations/Other_loops.js
@@ -10,7 +10,7 @@ const greetings = "Hello World"
 
 // for(const greet of greetings){
 //     if(greet == " ")
-//         continue
+//         break;
 //     console.log(greet)
 // }
 
@@ -20,7 +20,7 @@ const greetings = "Hello World"
 // map.set("USA", "United States of America")
 // map.set("UK", "United Kingdom")
 
-// for(const [key, value] of map){
+// for(const [key, value] of map){ // [key, value] - destructuring array.
 //     console.log(key, ":-", value)
 // }
 
@@ -111,5 +111,44 @@ const myCoding = [
 ]
 
 myCoding.forEach( (item) => {
-    console.log(item.languageFileName);
+    // console.log(item.languageFileName);
 } )
+
+
+// Filter on Array
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNum = myNums.filter( (num) => num > 4) // Filter will return new array.
+
+// const newNum = myNums.filter( (num) => {return num > 4}) // if you use curly braces in callback function you have to return the value. otherwise filter will return empty array
+
+// if you want to use for-Each loop.
+
+// const newNum = []
+
+// myNums.forEach( (num) => {
+//     if(num > 4){
+//         newNum.push(num)
+//     }
+// })
+
+// console.log(newNum);
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  let userBooks = books.filter( (bk) => bk.publish >= 2000)
+
+  console.log(userBooks);
+  
+  
